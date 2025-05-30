e0a61a76570370b2bdae69b8b6cba44bfa062578
Pablo Nunga <pablonungaray@protonmail.com>
2025-05-30 00:29:46 -0600

    Proyecto 7: Intro a Node.js y npm

---
+++ Proyectos/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,25 @@
+const planetas = 
+[
+    {
+        nombre: "Titán",
+        descripcion: "La luna más grande de Saturno, con lagos de metano.",
+        descubiertoEn: "1655"
+    },
+    {
+        nombre: "Próxima Centauri b",
+        descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+        descubiertoEn: "2016"
+    },
+    {
+        nombre: "Kepler-5b",
+        descripcion: "Planetas extrasolar descubierto por la misión Kepler.",
+        descubiertoEn: "2010"
+    },
+    {
+        nombre: "Neptuno",
+        descripcion: "Planeta gaseoso gigantes, también conocidos como gigante de hielo.",
+        descubiertoEn: "1846"
+    }
+    ];
+
+module.exports = planetas;
